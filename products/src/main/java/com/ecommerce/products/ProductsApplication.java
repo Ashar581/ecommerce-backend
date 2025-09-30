9d603dbd2e8c86004009762448b47ce39c64640e
Ashar581 <asharshahab@gmail.com>
2025-10-01 01:24:30 +0530

    Add & Change: Changed the commons to jar packaging, it is just used as bean injection. Injected the dependency in all microservices. Added component scan in all microservices

--- products/src/main/java/com/ecommerce/products/ProductsApplication.java
+++ products/src/main/java/com/ecommerce/products/ProductsApplication.java
@@ -2,7 +2,29 @@ package com.ecommerce.products;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = {
+        "com.ecommerce.commons.configs",
+        "com.ecommerce.commons.connectors",
+        "com.ecommerce.commons.controllers",
+        "com.ecommerce.commons.enums",
+        "com.ecommerce.commons.events",
+        "com.ecommerce.commons.exception",
+        "com.ecommerce.commons.exception.handler",
+        "com.ecommerce.commons.model",
+        "com.ecommerce.commons.model.auth",
+        "com.ecommerce.commons.model.base",
+        "com.ecommerce.commons.orders",
+        "com.ecommerce.commons.payments",
+        "com.ecommerce.commons.products",
+        "com.ecommerce.commons.security",
+        "com.ecommerce.commons.security.jwt",
+        "com.ecommerce.commons.security.jwt.config",
+        "com.ecommerce.commons.security.jwt.filter",
+        "com.ecommerce.commons.users",
+        "com.ecommerce.commons.utils"
+})
 @SpringBootApplication
 public class ProductsApplication {
 
